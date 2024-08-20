ea1e45a992a6bac2b1dd400a30813a20000360ab
AnanthAms <ananth.in95@gmail.com>
2024-08-20 11:50:34 +0530

    first commit

---
+++ src/main/java/com/example/vanakam/VanakamController.java
@@ -0,0 +1,11 @@
+package com.example.vanakam;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VanakamController {
+
+	public String getMessage() {
+		return "Yaar endru purigiradha-ivan thee endru therigiradha";
+	}
+}
